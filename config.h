a16cbe4c74316e1c6e7586ad9641a50693c5dd1f
Graphity <rohood3@gmail.com>
2022-05-10 02:33:05 +0400

    Patch font2

--- config.h
+++ config.h
@@ -6,6 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
